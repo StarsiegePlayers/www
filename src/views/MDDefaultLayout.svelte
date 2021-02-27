495264aacb2bc54afe5c731a4636221f13fc6753
Neo-Desktop <me@amrit.pw>
2021-02-27 01:09:43 -0800

    update default layout to support a last updated time for the server browser

--- src/views/MDDefaultLayout.svelte
+++ src/views/MDDefaultLayout.svelte
@@ -1,11 +1,13 @@
 <script>
     import Icon from "fa-svelte";
-    import {faEdit} from "@fortawesome/free-solid-svg-icons"
+    import {faEdit} from "@fortawesome/free-solid-svg-icons";
+    import {timeago} from "../components/timeago";
     import config from "../store/config";
 
     export let title;
     export let file;
     export let showEditLink = true;
+    export let LastUpdated = 0;
 </script>
 
 <style>
@@ -19,6 +21,9 @@
     {#if showEditLink}
         <div class="ms-auto align-self-end"><a href="{config.GitContentURL}/tree/main/{file}" target="_blank">Edit me on
             GitHub!<span class="push-left"><Icon icon={faEdit}/></span></a></div>
+    {/if}
+    {#if !showEditLink && LastUpdated > 0}
+        <div class="ms-auto align-self-end">Last Updated: <i use:timeago datetime="{LastUpdated}" locale="en_US">{LastUpdated}</i></div>
     {/if}
 </div>
 
